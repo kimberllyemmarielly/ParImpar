4fb48c9ff8d4e2e7cd0fb20564ca80bce2a9b0aa
Kimberly <krodriguessilva53@gmail.com>
2022-04-29 19:44:22 -0300

    finaliza tarefa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string paridade;
+
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            paridade = (numero % 2) == 0 ? "par" : "ímpar";
+
+            Console.WriteLine($"{numero} é {paridade}");
+        }
+    }
+}
